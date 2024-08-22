efb6a8612bece84461e2017458478cb26c2fa5ad
kishlay-notabot <kishlay.work1@gmail.com>
2024-08-22 21:53:09 +0530

    revert

--- main.py
+++ main.py
@@ -1,32 +1,16 @@
 import socket
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
  
 # Define the UDP IP address and port to listen on
 UDP_IP = "0.0.0.0"
 UDP_PORT = 4210
-fig = plt.figure()
-ax = fig.add_subplot(1,1,1)
-xs = []
-ys = []
-def animate(i, xs, ys, val):
-    xs.append(i)
-    ys.append(val)
-    xs = xs[-20:]
-    ys = ys[-20:]
-    ax.clear()
-    ax.plot(xs,ys)
  
 # Create a UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
  
 print(f"Listening for UDP packets on {UDP_IP}:{UDP_PORT}")
+ 
 while True:
     # Receive data from the socket
     data, addr = sock.recvfrom(1024)
-    dcd = data.decode('utf-8')
-    ani = animation.FuncAnimation(fig, animate, fargs=(xs,ys,dcd), interval=1)
-    dcd = dcd
-    plt.show()
-    print(dcd)
+    print(data.decode('utf-8'))
