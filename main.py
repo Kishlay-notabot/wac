5590b637872242a69a4e0b2242a1a40661ec7a1d
kishlay-notabot <kishlay.work1@gmail.com>
2024-08-22 21:44:25 +0530

    it works, but var value is fixed

--- main.py
+++ main.py
@@ -9,15 +9,24 @@ fig = plt.figure()
 ax = fig.add_subplot(1,1,1)
 xs = []
 ys = []
-
+def animate(i, xs, ys, val):
+    xs.append(i)
+    ys.append(val)
+    xs = xs[-20:]
+    ys = ys[-20:]
+    ax.clear()
+    ax.plot(xs,ys)
  
 # Create a UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
  
 print(f"Listening for UDP packets on {UDP_IP}:{UDP_PORT}")
- 
 while True:
     # Receive data from the socket
     data, addr = sock.recvfrom(1024)
-    print(data.decode('utf-8'))
+    dcd = data.decode('utf-8')
+    ani = animation.FuncAnimation(fig, animate, fargs=(xs,ys,dcd), interval=1)
+    dcd = dcd
+    plt.show()
+    print(dcd)
